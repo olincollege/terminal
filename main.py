206051a80115af405dec24759dfc93c8b7e2859e
Anna Du <adu@olin.edu>
2025-04-21 16:16:01 -0400

    displays only unlocked layer

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import curses
-import os
-
 from model import Model
 from view import View
-from controller import Controller
 
 
-def main():
-    print("hey there")
-
-    stdscr = curses.initscr()
-
-    game_model = Model()
-    game_view = View(stdscr)
-    game_controller = Controller()
+def main(stdscr):
+    model = Model()
+    view = View(stdscr)
+    accessible_artifacts = model.get_accessible_artifacts()
+    view.display_directory(accessible_artifacts)
 
 
 if __name__ == "__main__":
-    main()
+    curses.wrapper(main)
--- view.py
+++ view.py
@@ -1,6 +1,7 @@
 import time
 from curses import wrapper
 import curses
+import os
 
 
 class View:
@@ -9,23 +10,33 @@ class View:
         self._stdscr = stdscr
         self._pad = curses.newpad(100, 100)
 
-    def display_directory(self, directory):
+    def display_directory(self, accessible_artifacts):
+        """
+        Displays all files from the accessible artifact layers.
+
+        Args:
+            accessible_artifacts (list): List of accessible artifact layer names.
+        """
         self._stdscr.clear()
-        x = 0
-        y = 0
-        for file in directory:
-            self._pad.addstr(y, x, file.name)
-            y += 1
-
-        y += 1
-        self._pad.move(y, x)
-        self._pad.refresh(0, 0, 0, 0, 20, 75)
-
-        while True:
-            self._pad.addstr(self._pad.getkey())
-
-            self._pad.refresh(0, 0, 0, 0, 20, 75)
-            time.sleep(0.02)
+        row = 0
+        for layer in accessible_artifacts:
+            layer_path = os.path.join("artifacts", layer)
+            if os.path.isdir(layer_path):
+                files = [
+                    f
+                    for f in os.listdir(layer_path)
+                    if os.path.isfile(os.path.join(layer_path, f))
+                ]
+                self._stdscr.addstr(row, 0, f"{layer}/")
+                row += 1
+                for file in files:
+                    self._stdscr.addstr(row, 2, file)
+                    row += 1
+            else:
+                self._stdscr.addstr(row, 0, f"{layer}/ (Not Found)")
+                row += 1
+        self._stdscr.refresh()
+        self._stdscr.getch()  # Wait for user input
 
     def display_file(self, file):
         file.display()
